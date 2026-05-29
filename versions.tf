edf3371ab9c659842c00a4c0c7b48c427fad56b4
Mamadou Wane <228775663+mamadou-wane@users.noreply.github.com>
2026-05-29 17:45:00 -0400

    Configure S3 remote state backend

--- versions.tf
+++ versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.11.0"
 
+  backend "s3" {
+    bucket       = "sapper-tfstate-mw"
+    key          = "sapper/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
